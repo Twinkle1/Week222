310aaa6307fa18c27b04605efc141313a4a7d959
Twinkle1 <lanou3g@lanou3gdeMac-mini-7.local>
2015-05-22 09:33:07 +0800

    这是我的新工程

---
+++ ethjas/ethjas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ethjas
+//
+//  Created by lanou3g on 15/5/22.
+//  Copyright (c) 2015年 孟凡军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
